1c396f6b5aac567ec1c7eb09e0136d058b9d9087
nilleb <me@nilleb.com>
2015-02-09 21:52:56 +0100

    simplest implementation

--- rootaction-example-plugin/src/main/java/org/jenkinsci/plugins/rootactionexampleplugin/MyRootAction.java
+++ rootaction-example-plugin/src/main/java/org/jenkinsci/plugins/rootactionexampleplugin/MyRootAction.java
@@ -1,14 +1,19 @@
 package org.jenkinsci.plugins.rootactionexampleplugin;
 
 import hudson.Extension;
+import hudson.model.Describable;
+import hudson.model.Descriptor;
 import hudson.model.RootAction;
-
+import hudson.util.ListBoxModel;
+import java.util.ArrayList;
+import java.util.List;
 
 @Extension
-public class MyRootAction implements RootAction {
+public class MyRootAction implements RootAction, Describable<MyRootAction> {
+
     @Override
     public String getIconFileName() {
-        
+
         return "/images/32x32/blue.png";
     }
 
@@ -19,6 +24,55 @@ public class MyRootAction implements RootAction {
 
     @Override
     public String getUrlName() {
-        return "rootactionExamplePlugin";
+        return "MyRootAction";
+    }
+
+    @Override
+    public Descriptor getDescriptor() {
+        return new MyDescriptor();
+    }
+
+    public class MyGoal {
+
+        public final String displayName;
+
+        public String getDisplayName() {
+            return displayName;
+        }
+
+        public final String id;
+
+        public String getId() {
+            return id;
+        }
+
+        public MyGoal(String displayName, String id) {
+            this.id = id;
+            this.displayName = displayName;
+        }
+    }
+
+    public List<MyGoal> getBuildGoals() {
+        ArrayList<MyGoal> arrayList = new ArrayList<MyGoal>();
+        arrayList.add(new MyGoal("A", "A"));
+        arrayList.add(new MyGoal("B", "B"));
+        arrayList.add(new MyGoal("C", "C"));
+        return arrayList;
+    }
+    
+    public class MyDescriptor extends Descriptor<MyRootAction> {
+
+        @Override
+        public String getDisplayName() {
+            return "MyRootAction";
+        }
+
+        public ListBoxModel doFillGoalTypeItems() {
+            ListBoxModel items = new ListBoxModel();
+            for (MyGoal goal : getBuildGoals()) {
+                items.add(goal.getDisplayName(), goal.getId());
+            }
+            return items;
+        }
     }
 }
